47ec22fd318494b984255bfea05cc343bad2cbe1
himalaybhardwaj <169162360+himalaybhardwaj@users.noreply.github.com>
2026-06-27 04:05:23 +0000

    variables

---
+++ 01_basics/1variables.js
@@ -0,0 +1,21 @@
+const accountId = 23456;
+let account_email = "himalaya@gmail.com";
+var account_password = "23456"
+accountcity = "delhi";
+
+
+//accountId =2;  // it is constant no change
+// console.log(accountId)
+account_email = "hi@gmail.com";
+accouint_password = "111110";
+accountcity = "bengkuru";
+
+console.table([ accountId,account_email,account_password,accountcity]);
+
+
+
+// const - no change 
+/*
+prefer not to use var 
+because of block scope and fuynctional scope
+*/
